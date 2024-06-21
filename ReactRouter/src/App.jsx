5c89b3691211d7dd405381d99a9e95af8f7e1cff
CalebScott0 <calebbenscott@gmail.com>
2024-06-20 21:44:59 -0500

    updated styling and added footer

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import MainSection from "./components/MainSection";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
       <div id="container">
         <Navbar />
         <MainSection />
+        <footer>
+        <Link to="/">Home</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        </footer>
       </div>
   );
 }
